25b36f1d945a904578a8166b945841c9264a4530
francocasafus22 <francocasafus55@gmail.com>
2025-06-10 14:51:21 -0300

    fix login: arreglado

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 function login(usuario, clave) {
-    if (usuario === "admin" && clave === "1234") {
-        return false; // ERROR: debe devolver true
-    }
-    return true;
+  if (usuario === "admin" && clave === "1234") {
+    return true; // ERROR: debe devolver true
+  }
+  return false;
 }
